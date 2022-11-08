17b248689b93512dbd92de3f5b20887961891099
priscila.teodoro <priteodoro01@gmail.com>
2022-11-07 21:28:28 -0400

    Iniciando construtor

---
+++ Filas.java
@@ -0,0 +1,15 @@
+/**
+ * filas
+ */
+public class Filas {
+    
+    public int[] tamanho;
+
+    public Filas (int tamanho) {
+        this.tamanho = new int[tamanho];
+    }
+
+    public Filas() {
+        this.tamanho = new int[5];
+    }
+}
