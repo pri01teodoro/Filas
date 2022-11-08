25499d392c53abfd9dfffa4047d29d9d34de1dd0
priscila.teodoro <priteodoro01@gmail.com>
2022-11-07 22:30:29 -0400

    Implementando métodos size, isEmpty e front

---
+++ EmptyQueueException.java
@@ -0,0 +1,5 @@
+public class EmptyQueueException extends RuntimeException{
+    public EmptyQueueException(String excecao) {
+        super(excecao);
+    }
+}
--- Filas.java
+++ Filas.java
@@ -2,14 +2,32 @@
  * filas
  */
 public class Filas {
-    
-    public int[] tamanho;
 
-    public Filas (int tamanho) {
-        this.tamanho = new int[tamanho];
+    public int[] array;
+    public int primeiro = 0;
+    public int ultimo = 0;
+    
+
+    public Filas(int tamanho) {
+        this.array = new int[tamanho];
     }
 
     public Filas() {
-        this.tamanho = new int[5];
+        this.array = new int[5];
+    }
+
+    public int size() {
+        return((this.array.length - primeiro + ultimo)%this.array.length);
+    }
+
+    public boolean isEmpty() {
+        return (primeiro == ultimo);
+    }
+
+    public int front() {
+        if (isEmpty()) {
+            throw new EmptyQueueException("Fila vazia");
+        }
+        return array[primeiro];
     }
 }
