ecc7baee02a5f01e01a745fbe4e2a6d1994690de
pri01teodoro <priteodoro01@gmail.com>
2022-11-15 23:12:18 -0300

    Adicionando todos os métodos

--- Filas.java
+++ Filas.java
@@ -3,31 +3,52 @@
  */
 public class Filas {
 
-    public int[] array;
-    public int primeiro = 0;
-    public int ultimo = 0;
+    public Integer[] fila;
+    public Integer primeiro = 0;
+    public Integer ultimo = 0;
+    public int tamanho = 5;
     
 
-    public Filas(int tamanho) {
-        this.array = new int[tamanho];
-    }
-
     public Filas() {
-        this.array = new int[5];
+        this.fila = new Integer[5];
     }
 
-    public int size() {
-        return((this.array.length - primeiro + ultimo)%this.array.length);
+    public Filas(int tamanho) {
+        this.fila = new Integer[this.tamanho];
+    }
+
+    public Integer size() {
+        return((tamanho - primeiro + ultimo)%tamanho); //this.fila.length
     }
 
     public boolean isEmpty() {
         return (primeiro == ultimo);
     }
 
-    public int front() {
+    public Integer front() {
         if (isEmpty()) {
             throw new EmptyQueueException("Fila vazia");
         }
-        return array[primeiro];
+        return fila[primeiro];
+    }
+
+    public Integer dequeue() {
+        if (isEmpty()) {
+            throw new EmptyQueueException("Fila vazia");
+        }
+
+        Integer elemento = fila[primeiro];
+        fila[primeiro] = null;
+        primeiro = (ultimo + primeiro)%tamanho;
+
+        return elemento;
+    }
+
+    public void enqueue(Integer elemento){
+        if (size() == tamanho - 1) {
+            throw new FullQueueException("Fila cheia");
+        }
+        fila[ultimo] = elemento;
+        ultimo = (ultimo + 1)%tamanho;
     }
 }
---
+++ Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        Filas fila =  new Filas(10);
+        
+        fila.enqueue(1);
+        fila.enqueue(2);
+        fila.enqueue(3);
+
+        // System.out.println(fila.size());
+        // System.out.println(fila.isEmpty());
+
+        System.out.println(fila.dequeue());
+        // System.out.println(fila.dequeue());
+        System.out.println(fila.size());
+    }
+}
